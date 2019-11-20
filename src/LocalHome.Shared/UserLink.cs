0ee598cc5770d7bbe78c63e8fa8cd6f0a5eb3244
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-11-19 23:03:39 -0500

    wip

--- src/LocalHome.Shared/UserLink.cs
+++ src/LocalHome.Shared/UserLink.cs
@@ -4,5 +4,6 @@ namespace LocalHome.Shared {
         public string Text { get; set; }
         public string ImageUrl { get; set; }
         public string Url { get; set; }
+        public bool InvertImageColors { get; set; }
     }
 }
