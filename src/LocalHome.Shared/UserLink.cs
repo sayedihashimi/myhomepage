fb6bdf8140803109457e3ca3e9ef6a242822f3e2
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-11-22 15:54:00 -0500

    wip

--- src/LocalHome.Shared/UserLink.cs
+++ src/LocalHome.Shared/UserLink.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalHome.Shared {
     public class UserLink : IUserLink {
+        [Required]
         public string Text { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+        [Required]
         public string Url { get; set; }
         public bool InvertImageColors { get; set; }
     }
